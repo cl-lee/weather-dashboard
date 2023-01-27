65513b81edb6746cb4e63c49cb2cba2e4e751136
cl.lee <cl.lee.ofz@gmail.com>
2023-01-27 18:58:12 +0000

    added codes to get city name, today's date, today's temperature and today's wind speed

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ searchForm.addEventListener("submit", function (event) {
     event.preventDefault();
     let chosenCity = searchInput.value;
     addCityToHistory(chosenCity);
-    todaysWeather(chosenCity);
+    getTodaysWeather(chosenCity);
 })
 
 // 2) save entered city into local storage (nest within (1))
@@ -30,12 +30,12 @@ function addCityToHistory(chosenCity) {
 //          historyContainer.prepend(historyButton);
 
 // 4) return today's weather
-function todaysWeather(chosenCity) {
+function getTodaysWeather(chosenCity) {
     //          (i) get lat and lon of city:
     //              API query URL:
-    let queryURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + chosenCity + "&limit=5&appid=7018058f3ae12d10c5b76a1ecf1894e9"
+    let geoDataURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + chosenCity + "&limit=5&appid=7018058f3ae12d10c5b76a1ecf1894e9"
 
-    fetch(queryURL)
+    fetch(geoDataURL)
         .then(response => response.json())
         .then(function (geoData) {
             let lat = geoData[0].lat
@@ -44,35 +44,51 @@ function todaysWeather(chosenCity) {
             //                  return fetch(queryURL2)
             //              (ii) get today's weather for city
             //                   API query URL:
-            console.log(`lat = ${lat}, lon = ${lon}`);
+            return fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=7018058f3ae12d10c5b76a1ecf1894e9`)
         })
-            //  let queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=7018058f3ae12d10c5b76a1ecf1894e9"
-            //  fetch(queryURL)
-            //  .then(response => response.json())
-            //  .then(function (response) {
+        .then(response => response.json())
+        .then(function (weatherData) {
             //  todayContainer.append(
             // `<h2>${cityInput} (${moment().format("D/M/YYYY")})</h2>
             // <p>Temp: 
+            console.log(weatherData);
+            
+            // get city name and today's date
+            let chosenCityName = weatherData.city.name;
+            let todaysDate = moment().format("D/M/YYYY");
+            
+            // get today's temperature
+            let todaysTemperatureInKelvin = weatherData.list[0].main.temp;
+            let todaysTemperatureInCelsius = todaysTemperatureInKelvin-273.15;
+            let todaysTemperatureRounded = Math.round(todaysTemperatureInCelsius*100)/100;
+            let todaysTemperature = `${todaysTemperatureRounded}°C`;
+
+            // get wind speed
+            let todaysWindSpeedMPS = weatherData.list[0].wind.speed;
+            let todaysWindSpeedKPH = todaysWindSpeedMPS*3.6;
+            let todaysWindSpeedRounded = `${Math.round(todaysWindSpeedKPH*100)/100} KPH`;
+            console.log(todaysWindSpeedRounded);
+
+        });
 
 
 
+    // `
+    // )        
 
-            // `
-            // )        
 
+    // let pickedCity = document.createElement("h2")
+    //      pickedCity.textContent = "cityInput"+" ("+moment().format("D/M/YYYY");
+    //      let todaysTemperature = document.createElement()
+    //  })
+    //              })
+    //                  
+    //      
+    // 
+    // 
+    // 
 
-            // let pickedCity = document.createElement("h2")
-            //      pickedCity.textContent = "cityInput"+" ("+moment().format("D/M/YYYY");
-            //      let todaysTemperature = document.createElement()
-            //  })
-            //              })
-            //                  
-            //      
-            // 
-            // 
-            // 
-
-        }
+}
 
 
 // 5) return 5 day forecast

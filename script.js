9c126b41239c1875935c1ce7d94f3a3436abaf81
cl.lee <cl.lee.ofz@gmail.com>
2023-01-27 09:52:40 +0000

    added some pseudo codes for application functions

---
+++ script.js
@@ -0,0 +1,32 @@
+// --- Global Variable ---
+//let historyContainer = document.querySelector("#history"); 
+
+// application functions:
+// 1) enter city in textbox
+//      add eventlistener on "click" for city search
+//      if form: add preventDefault();
+//      let #search-input.value = cityInput
+// 2) add city to history section
+//      (i) add cities history to corresponding array in local storage (on click)
+//      create: function addToHistory();     
+//      declare cityHistory as array, or retrieve from localStorage's citiesHistoryList if available    
+//      create array: let citiesHistory = JSON.parse(localStorage.getItem [];
+//      limit number of cities in history? (citiesHistory.length);
+//      citiesHistory.push(cityInput);
+
+// 
+//      on eventlistener "click" for city search: 
+//          for loop:
+//          let historyButton = document.createElement("button");
+//          historyButton.textContent = citiesHistory[i];
+//          historyContainer.prepend(historyButton);
+// 
+//       
+
+// 3) return today's weather
+//      create function     
+// 
+//      create function todaysWeather() = 
+//          
+
+// 4) return 5 day forecast

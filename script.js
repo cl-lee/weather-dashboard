8a1e336d3c328f36d2dc716b35a76c6b5c242ce0
cl.lee <cl.lee.ofz@gmail.com>
2023-01-31 17:09:03 +0000

    fixed test code

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ function addCityToHistory(selectedCity) {
 // Renders the six most recently searched cities in sidebar
 function renderSearchHistory() {
     let searchHistoryArray = JSON.parse(localStorage.getItem("citySearchHistory")) || [];
-    console.log(test);
+    console.log("test");
     historyContainer.innerHTML =
         `<a href="#" class="list-group-item list-group-item-action active">${searchHistoryArray[0] || ""}</a>
         <a href="#" class="list-group-item list-group-item-action active">${searchHistoryArray[1] || ""}</a>
